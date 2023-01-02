5e6a8f6966e884f27661e8a0dc30c3e684acda5a
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-01-02 12:18:47 +0100

    try

--- data_acquisition/Héloïse/try.py
+++ data_acquisition/Héloïse/try.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 url = "https://www.immoweb.be/en/search/house/for-sale?countries=BE&page=2&orderBy=relevance"
 r = requests.get(url)
@@ -7,5 +8,13 @@ soup = BeautifulSoup(r.content, features="html.parser")
 content = r.content
 
 links = []
-for elem in soup.find_all("div", attrs={"class": "results"}):
-    print(elem)
+for elem in soup.find_all("div", attrs={"class": "results"}) :
+    test = str(elem)
+    links.append(test)
+
+
+test = f"./datas.json"
+with open(test, 'w') as file:
+    json.dump(links, file, indent=4)
+
+
