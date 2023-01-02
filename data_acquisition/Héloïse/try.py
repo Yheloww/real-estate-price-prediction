a987c27b7718bbd53392d8e0ed698452e5e01d28
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-01-02 15:14:24 +0100

    continu

--- data_acquisition/Héloïse/try.py
+++ data_acquisition/Héloïse/try.py
@@ -6,15 +6,20 @@ url = "https://www.immoweb.be/en/search/house/for-sale?countries=BE&page=2&order
 r = requests.get(url)
 soup = BeautifulSoup(r.content, features="html.parser")
 content = r.content
-
+print(content)
 links = []
-for elem in soup.find_all("div", attrs={"class": "results"}) :
-    test = str(elem)
-    links.append(test)
+for elem in soup.find_all("li", ):
+    print(elem.get("href"))
+    # test = elem.text
+    # links.append(test)
+for elem in soup.find_all("a", id="card__title-link") :
+    print(elem)
+    print(elem.get("id href"))
 
 
-test = f"./datas.json"
-with open(test, 'w') as file:
-    json.dump(links, file, indent=4)
+
+# test = f"./datas.json"
+# with open(test, 'w') as file:
+#     json.dump(links, file, indent=4)
 
 
