fea6462153fb68eb4360f1092eb3cada991c1d59
AnhSN <AnhSN@users.noreply.github.com>
2023-01-05 10:06:25 +0100

    trying to incorporate Heloise's functions

--- data_acquisition/Anh/scrapper_Anh.py
+++ data_acquisition/Anh/scrapper_Anh.py
@@ -1,35 +1,60 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import pandas as pd
-from csv import writer
-import re
 import json
+import pandas as pd
+import numpy as np
+from concurrent.futures import ThreadPoolExecutor
+import time
+from csv import writer
 
-# https://www.immoweb.be/en/search/house-and-apartment/for-sale/brussels/province?countries=BE&page=1&orderBy=relevance
+# currently not working at some points
 
-# provinces list
-provinces_list=["antwerp", "brabant", "brussels", "west-flanders", "east-flanders", "hainaut", "liege", "limburg", "luxembourg", "namur"]
-for y in range(len(provinces_list)):
-    province=provinces_list[y]
-    # page number
-    for i in range(1, 2):
-        page_num = str(i) + "&orderBy=relevance"
-        url = (
-            "https://www.immoweb.be/en/search/house-and-apartment/for-sale/"+province+"/province?countries=BE&page="+page_num
-        )
-        list_of_properties = []
+# json creation
+doc = f"./test.json"
 
-        driver = webdriver.Firefox()
-        driver.get(url)
+# webdriver setup
+def driversetting():
+    driver = webdriver.Firefox()
+    return driver
 
-        soup = BeautifulSoup(driver.page_source, "html.parser")
-        listings = soup.find_all("a", class_="card__title-link")
+# scraping
+def scraping(iteration, driver):
+    list_of_properties = []
+    provinces = ["antwerp", "brabant", "brussels", "west-flanders", "east-flanders", "hainaut", "liege", "limburg", "luxembourg", "namur"]
 
-        for pages in listings:  
+    for prov in range(len(provinces)):
+        province=provinces[prov]
+        for number in iteration:
+            page_num = str(number)
+            url = ("https://www.immoweb.be/en/search/house-and-apartment/for-sale/"+province+"/pronvince?countries=BE&page="+page_num+"&orderBy=relevance")
 
-            property_details = {}
-            driver.get(pages["href"]) 
-            property_details_page = BeautifulSoup(driver.page_source, "html.parser")
+            driver.get(url)
 
-            script_list = property_details_page.find_all("script")
-            print(script_list)
+            soup = BeautifulSoup(driver.page_source, "html.parser")
+
+            listings = soup.find_all("a", class_="card__title-link")
+            for pages in listings:  
+                driver.get(pages["href"]) # get the response from the url
+                property_details_page = BeautifulSoup(driver.page_source, "html.parser")
+                content = property_details_page.find("section", class_="classified_section")
+                list_of_properties.append(content)
+        return list_of_properties
+
+# json writing
+def saving_datas(datas):
+    with open(doc, 'a') as file:
+        json.dump(str(datas), file, indent=4)
+
+# concurrency setting
+def pool():
+    # 1 driver by thread (we have four thread here)
+    drivers = [driversetting() for _ in range(8)]
+    #division of the number of pages by the number of threads 
+    division = np.array_split(np.arange(1,8),8)
+    #creation of a pool of threads
+    with ThreadPoolExecutor(max_workers=8) as executor :
+        results = executor.map(scraping,division,drivers)
+        for result in results: 
+            saving_datas(result)
+
+pool()
