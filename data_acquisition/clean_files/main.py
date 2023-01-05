3af4b7cf3de044cf178945eb33d0ab103bbe56e5
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-01-05 16:03:12 +0100

    assembly file

---
+++ data_acquisition/clean_files/main.py
@@ -0,0 +1,10 @@
+from get_links import *
+from threads import * 
+from scrapin_data import *
+
+
+
+pool(2,4)
+
+data = organise_data(clean_data())
+
--- data_acquisition/clean_files/scrapin_data.py
+++ data_acquisition/clean_files/scrapin_data.py
@@ -1,43 +1,101 @@
 from get_links import *
-
+import pandas as pd
 
 def clean_data():
-    house_number = 1
-    dict_of_informations= {}
+
     list_of_link = "./links.txt"
     my_file = open(list_of_link, "r")
     for lk in my_file : 
         r = requests.get(lk)
         content = r.content
         property_details_page = BeautifulSoup(content,"html.parser")
-        dict_of_data= {}
-        #we clean the datas by finding tags
-        for tr in property_details_page.find_all('tr', attrs={'class':'classified-table__row'}):
-            #these are the tage were our infos are
-            value = tr.find('th')
-            data = tr.find('td')
-            if value != None :
-                #we clean them using regex yay !
-                title = re.sub(r'\s*<[^>]*>\s*','',str(value))
-                if data != None : 
-                    datas = re.sub(r'\s*<[^>]*>\s*','',str(data))
-                    # we delete the big space to replace them with 1 only
-                    datass = " ".join(datas.split())
-                    #we store data in the dict
-                    dict_of_data[title] = datass
-                else: 
-                    #if there are no datas we write none
-                    dict_of_data[title] = None
-            else: 
-                continue
-        #we put the datas in a another dict with all the house listed
+        script_list = property_details_page.find_all("script")
         
+    datas = organise_data(script_list)
+    csv = save_to_csv(datas)
+    return csv
 
-        dict_of_informations[house_number] = dict_of_data
-        house_number += 1 
-    print(dict_of_informations[1])
-    print(dict_of_informations[2])
-    return dict_of_informations
-    #right now the data is are dictionaries in a dictionary
+def organise_data(scripts): 
+    list_of_properties = []
+    for script in scripts:  # iterate all the scripts to get relevent information
 
-clean_data()
+        # search for the script tag which have window.dataLayer in it, This script have most 
+        # of the basic details that we need.
+
+        if "window.dataLayer" in str(script): 
+            script_content = str(script)
+            details_json = json.loads(
+                script_content[
+                    script_content.index("[") : script_content.index("]") + 1
+                ]
+            )
+
+        # search for the script tag which have window.classified in it, This script have some more 
+        # information which we not found in previous script of the basic details that we need.
+        
+        if "window.classified" in str(script):
+            script_content = str(script)
+            facade_count = ""
+            fireplace_exist = ""
+            isFurnished = ""
+            living_area = ""
+
+            # It was defficult to parse this string to json because of the special character's, 
+            # we have use split function to get required information from string.
+            if '"facadeCount":' in script_content:
+                facade_count = script_content.split('"facadeCount":')[1][
+                    :2
+                ].replace(",", "")
+            if '"fireplaceExists":' in script_content:
+                fireplace_exist = script_content.split('"fireplaceExists":')[1][
+                    :5
+                ].replace(",", "")
+            if '"isFurnished":' in script_content:
+                isFurnished = script_content.split('"isFurnished":')[1][:5].replace(
+                    ",", ""
+                )
+            if '"netHabitableSurface"' in script_content:
+                living_area = (
+                    script_content.split('"netHabitableSurface":')[1]
+                ).split(",")[0]
+
+        element_locality = (script.find("span", class_="classified__information--address-row")
+        if element_locality == None:
+            .text.replace("\n", "")
+            .strip()
+            .replace("           ", "  ")
+        )
+
+        property_details = {}
+
+        for key in ("type", "subtype", "price", "transactionType", "bedroom", "kitchen", "land", "building"):
+            property_details[key.title()] = (
+                None if details_json[0]["classified"][key] == "" else details_json[0]["classified"][key]
+            )
+
+        # Set property_details keys to 1 if the corresponding value in details_json[0]["classified"] is "true", else set the key to 0
+        for key in ("isFurnished", "fireplace_exist", "garden", "terrace", "swimming_pool"):
+            property_details[key.title()] = 1 if details_json[0]["classified"][key] == "true" else 0
+
+        # Set property_details keys to the corresponding value in details_json[0]["classified"]
+        # if the value is not an empty string, else set the key to None
+        for key in ("locality", "surface_area", "living_area", "facade_count"):
+            property_details[key.title()] = None if details_json[0]["classified"][key] == "" else details_json[0]["classified"][key]
+
+        list_of_properties.append(property_details)
+    print(list_of_properties)
+    return list_of_properties
+
+    
+
+def save_to_csv(data): 
+
+    datas = data
+    df = pd.DataFrame(datas)
+    df.replace("", None, inplace=True)
+
+        # Saving the dataframe into a CSV file 
+        # We specify mode (append) so the data will be appended into the csv file for every webpage we scrape
+    
+    return df.to_csv("property_data.csv", mode="a", header=None, index=False)
+       
