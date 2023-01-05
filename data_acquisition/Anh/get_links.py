381ec8f9e1aa1946af333bbe2314096627dac5ed
AnhSN <anhs.noel@gmail.com>
2023-01-05 16:53:14 +0100

    adding a pagination function

--- data_acquisition/Anh/get_links.py
+++ data_acquisition/Anh/get_links.py
@@ -1,6 +1,3 @@
-
-#this jupiter notebook rassemble my work in order
-#first we import every libraries that we need
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
@@ -8,19 +5,29 @@ import json
 import requests
 import numpy as np
 from concurrent.futures import ThreadPoolExecutor
-#then we create the file were the datas are gonna go
+
+# destination file of the URLs collected
 test = f"./links.txt"
-#we initiate the driver for the selenium part
+
+# driver init
 def driver_gen():
     driver = webdriver.Firefox()
     return driver
 
+# detect if the page contains a pagination
+def pagination(search_page)
+    has_pagination = search_page.find("class":"pagination")
+    if has_pagination == -1:
+        page_exists = False
+    else:
+        page_exists = True
+    return page_exists
 
+# open immoweb using selenium and add the links of announces into a list
 def get_links(iterations, driver):
     list_of_link = []
     provinces = ["antwerp", "brabant", "brussels", "west-flanders", "east-flanders", "hainaut", "liege", "limburg", "luxembourg", "namur"]
 
-    #still doing by page number is gonna change
     for prov in range(len(provinces)):
         province=provinces[prov]
     
@@ -42,6 +49,7 @@ def get_links(iterations, driver):
     print(len(list_of_link))
     return list_of_link
 
+# save the list of links into an external file
 def link_file(datas):
     with open(test, 'a') as file:
         file.write(f'{datas}\n')
