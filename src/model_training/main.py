1cc140416cd92ae4799bfeeb556264fa633ec614
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-01-26 14:14:32 +0100

    final commit for ML

--- src/model_training/main.py
+++ src/model_training/main.py
@@ -1,9 +1,10 @@
-from model_train import * 
+from model_train import *
 from preprocessing import *
 
 PATH_RAW = r'C:\Users\feldm\Documents\GitHub\real-estate-price-prediction\data\raw.csv'
 
-def preprocess_ensemble(): 
+
+def preprocess_ensemble():
     """
     function to unite every function in preprocessing
     """
@@ -14,13 +15,12 @@ def main():
     get_raw_data(PATH_RAW)
     # process the datas
     Path = preprocess_ensemble()
-    # train and evaluate 
-    X_train,X_test, y_train, y_test = get_X_y(Path, "Price")
+    # train and evaluate
+    X_train, X_test, y_train, y_test = get_X_y(Path, "Price")
+    evalutation = eval_model(X_train, X_test, y_train, y_test)
 
-    evalutation = eval_model(X_train,X_test, y_train, y_test)
-         
     return evalutation
 
 
 if __name__ == "__main__":
-    main() 
+    main()
--- src/model_training/model_train.py
+++ src/model_training/model_train.py
@@ -1,5 +1,5 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
@@ -8,52 +8,80 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_percentage_error
-
-
 from sklearn.pipeline import Pipeline
 
 PATH = r'C:\Users\feldm\Documents\GitHub\real-estate-price-prediction\data\preprocessed.csv'
 
 
-def get_dataframe(Path : str):
+def get_dataframe(Path: str) -> pd.DataFrame:
     """
+    return a Pandas Dataframe from a relative path
 
+        Parameter:
+            Path (str): a relative path in your repo where the dataset is 
+
+        Returns: 
+            df (pd.dataframe) : a pandas object of your dataset to work on
     """
     df = pd.read_csv(Path)
-    return df 
+    return df
 
 
-def get_X_y(PATH, test : str):
+def get_X_y(PATH, test: str):
     """
-    
+     return training and testing set to fit the model
+
+        Parameter:
+            PATH (str): a relative path in your repo where the dataset is 
+            testn(str) : name of the column you want to predict 
     """
     df = get_dataframe(PATH)
 
     y = df[test].values
-    X = df.drop(test,axis=1).values
+    X = df.drop(test, axis=1).values
 
-    X_train,X_test, y_train, y_test = train_test_split(X,y,random_state=15)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=15)
 
-    return X_train,X_test, y_train, y_test
+    return X_train, X_test, y_train, y_test
 
 
-def model_training(X_train, y_train):
+def model_training(X_train: np.array, y_train: np.array):
     """
-    
+    return a pipeline with your model that is trained
+
+        Parameter:
+            X_train(np.array) : an numpy array that represent your training features 
+            y_train(np.array) : an numpy array that represent your training data
+
+
+        Returns: 
+            pipe (sklearn.pipeline) : the pipeline of your model
     """
-    steps=[("scale", StandardScaler()),
-        ("model", GradientBoostingRegressor(n_estimators=500, learning_rate=0.1, max_depth=9))]
+    steps = [("scale", StandardScaler()),
+             ("model", GradientBoostingRegressor(n_estimators=500, learning_rate=0.1, max_depth=9))]
 
     pipe = Pipeline(steps)
-    pipe.fit(X_train,y_train)
+    pipe.fit(X_train, y_train)
     print("model trained")
-    return pipe 
+    print(type(pipe))
+    return pipe
 
-def eval_model(X_train,X_test, y_train, y_test):
+
+def eval_model(X_train: np.array, X_test: np.array, y_train: np.array, y_test: np.array):
     """
-    
+      returns the different scores of your model( rsquared,rmse, mape)
+
+        Parameter:
+            X_train(np.array) : an numpy array that represent your training features 
+            y_train(np.array) : an numpy array that represent your training data
+            X_test(np.array) : an numpy array that represent your test features 
+            y_testnp.array) : an numpy array that represent your test data
+
+
+        Returns: 
+            str : formatted str that shows the scores together
     """
-    pipe = model_training(X_train,y_train)
+    pipe = model_training(X_train, y_train)
 
     y_pred = pipe.predict(X_test)
 
@@ -61,12 +89,11 @@ def eval_model(X_train,X_test, y_train, y_test):
 
     rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
-    mape = mean_absolute_percentage_error(y_test,y_pred)
-
+    mape = mean_absolute_percentage_error(y_test, y_pred)
 
     return f"r2 score is {r2}, the rsquared error is {rmse}, the mean absolute error % is {mape} "
 
 
-X_train,X_test, y_train, y_test = get_X_y(PATH, "Price")
 
-print(eval_model(X_train,X_test, y_train, y_test))
+X_train, X_test, y_train, y_test = get_X_y(PATH, "Price")
+print(eval_model(X_train, X_test, y_train, y_test))
