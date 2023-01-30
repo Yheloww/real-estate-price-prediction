ef9cbf0eb03e6b633c2ec8eee8a6a173c856389f
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-01-30 16:56:29 +0100

    "ich"

---
+++ Deployement/form.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,IntegerField, BooleanField, RadioField
+from wtforms.validators import DataRequired, InputRequired
+
+#class of the form 
+class TestForm(FlaskForm):
+    type_property = RadioField("What is the property type ?", 
+                               choices=['house','apartement'], 
+                               validators=[InputRequired()])
+    provinces = RadioField("What is the province ?", 
+                               choices=['Luxembourg','Liege','Namur'], 
+                               validators=[InputRequired()])
+    fire_place = BooleanField("Fire place ?", default='checked')
+    kitchen = BooleanField("is the kitchen fully equiped ?", 
+                           default='checked')
+    furnished = BooleanField("furnished ?", default='checked')
+    garden = BooleanField("is there a garden ?", default='checked')
+    swim = BooleanField("swimmingpool ?", default='checked')
+    terrace = BooleanField("terrace ?", default='checked')
+
+    number_bedroom = IntegerField("Number of rooms ?", validators=[DataRequired()])
+    living_area = IntegerField("Living area ?", validators=[DataRequired()])
+    facades_number = IntegerField("Number of facades ?", validators=[DataRequired()])
+
+    submit = SubmitField("submit")
--- Deployement/hello.py
+++ Deployement/hello.py
@@ -1,13 +1,25 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from form import TestForm
+import csv
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "password"
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template("login.html")
+infos_dict = {}
 
-@app.route("/greet")
-def greet():
-    return render_template("greet.html", name=request.args.get("name","world"))
+
+@app.route('/', methods=['GET','POST'])
+def name():
+    form = TestForm()
+    infos_dict = {}
+    if form.validate_on_submit():
+        infos_dict["type_property"] = form.type_property.data
+        infos_dict["fire_place"] = form.fire_place.data
+        infos_dict["Number_bedrooms"] = form.number_bedroom.data
+        return jsonify(list(infos_dict))
+    return render_template("form.html",
+                            form=form)
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
